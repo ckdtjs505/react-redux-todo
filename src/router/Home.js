62bf30d0b40c2d114480e0da607f6264c8b02080
ckdtjs505 <ckdtjs505@naver.com>
2021-06-28 21:26:33 +0900

    feat : set change

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function home() {
-  const [text, setText] = useState("");
+// Home 을 home으로 쓰니까 Hook이 에러나오네?
+const Home = () => {
+  const [list, setList] = useState("");
+
+  let text = "";
+  const setText = value => {
+    this.text = value;
+  };
+  const onChange = e => {
+    const value = e.currentTarget.value;
+    setText(value);
+  };
+  const onSubmit = e => {
+    e.preventDefault();
+    setText("");
+  };
   return (
     <>
       <h1>To Do</h1>
-      <form>
-        <input placeholder="값을 입력하시오"></input>
+      <form onSubmit={onSubmit}>
+        <input placeholder="값을 입력하시오" value={text} onChange={onChange}></input>
+        <input placeholder="값을 입력하시오" value={text}></input>
         <button> 등록 </button>
       </form>
       <ul></ul>
     </>
   );
-}
+};
 
-export default home;
+export default Home;
