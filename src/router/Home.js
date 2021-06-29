611baa0ce4157871417b48b44ce97113a66b2a93
ckdtjs505 <ckdtjs505@naver.com>
2021-06-29 21:29:41 +0900

    fix : input and redux

--- src/router/Home.js
+++ src/router/Home.js
@@ -3,25 +3,19 @@ import { useState } from "react";
 // Home 을 home으로 쓰니까 Hook이 에러나오네?
 const Home = () => {
   const [list, setList] = useState("");
-
-  let text = "";
-  const setText = value => {
-    this.text = value;
-  };
   const onChange = e => {
     const value = e.currentTarget.value;
-    setText(value);
+    setList(value);
   };
   const onSubmit = e => {
     e.preventDefault();
-    setText("");
+    setList("");
   };
   return (
     <>
       <h1>To Do</h1>
       <form onSubmit={onSubmit}>
-        <input placeholder="값을 입력하시오" value={text} onChange={onChange}></input>
-        <input placeholder="값을 입력하시오" value={text}></input>
+        <input placeholder="값을 입력하시오" value={list} onChange={onChange}></input>
         <button> 등록 </button>
       </form>
       <ul></ul>
