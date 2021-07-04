73c1dab62bf69db4befd7c4c3a542a3cb96b2a4b
ckdtjs505 <ckdtjs505@naver.com>
2021-07-04 23:25:31 +0900

    feat: Detail page

--- src/router/Detail.js
+++ src/router/Detail.js
@@ -1,1 +1,23 @@
-export default () => "Detail";
+import { connect } from "react-redux";
+
+const Detail = ({ list }) => {
+  console.log(list);
+  return (
+    <>
+      <h1>{list?.text}</h1>
+      <h5>create at : {list?.id}</h5>
+    </>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps);
+  const {
+    match: {
+      params: { id }
+    }
+  } = ownProps;
+  return { list: state.find(ele => ele.id === parseInt(id)) };
+};
+
+export default connect(mapStateToProps)(Detail);
