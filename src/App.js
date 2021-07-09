b2c55b7a7d01dd383f5b6fa48435ad1827483cba
ckdtjs505 <ckdtjs505@naver.com>
2021-07-09 20:28:30 +0900

    feat: todo 만들기 연습

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter, Route } from "react-router-dom";
 import Detail from "./router/Detail";
 import Home from "./router/Home";
+import HomeTest from "./router/HomeTest";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <HashRouter>
         <Route exact path="/" component={Home}></Route>
         <Route path="/:id" component={Detail}></Route>
+        <Route exact path="/test" component={HomeTest}></Route>
       </HashRouter>
     </div>
   );
