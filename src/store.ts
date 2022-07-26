3c9f28b666fcf3dd37f14abe1b46513c761eeb10
ckdtjs505 <ckdtjs505@naver.com>
2022-07-26 16:46:30 +0900

    🛠️fix : store type update

--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,25 @@
-import { CaseReducer, configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-type State = { text: string; id: number; }[];
+type State = { text: string, id: number }[]
 
 // 주의! : localstorage reduxState에 data 가 없는 경우 null로 선언되어 배열로 변경
-const localData : State = JSON.parse(localStorage.getItem("reduxState") || '[]');
+const localData: State = JSON.parse(localStorage.getItem("reduxState") || '[]');
 
-const initialState : State  = localData;
-const add: CaseReducer<State, PayloadAction<string>> = (state, action) => [{ text: action.payload, id: Date.now() }, ...state];
-const remove: CaseReducer<State, PayloadAction<string>> = (state,action) => state.filter(todo => todo.id !== parseInt(action.payload))
+const initialState = localData;
 
 const listSlice = createSlice({
   name : 'list',
   initialState,
   reducers: {
-    add,
-    remove 
+    add : (state,action) => [{ text: action.payload, id: Date.now() }, ...state],
+    delete : (state,action) => state.filter(todo => todo.id !== action.payload)
   }
 })
 
 export const actionCreator = {
   actionAdd : listSlice.actions.add,
-  actionDelete : listSlice.actions.remove
+  actionDelete : listSlice.actions.delete
 };
-
 
 const store = configureStore({
   reducer : listSlice.reducer,
